42509b18131e5ae9175267e2021f3f7f444979ea
Santino Wang <thraupis@outlook.com>
2024-03-28 17:15:58 +0800

    init

---
+++ src/TinyBenchmark/Program.cs
@@ -0,0 +1,85 @@
+using CommandLine;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace TinyBenchmark
+{
+    class Options
+    {
+        [Option('g', "group", Required = false, HelpText = "set the group number.")]
+        public int Group { get; set; } = 2;
+
+        [Option('r', "random", Required = false, HelpText = "Randomize counter.")]
+        public bool IsRandomCounter { get; set; } = false;
+    }
+
+    internal class Program
+    {
+        [DllImport("kernel32.dll")]
+        public static extern uint GetCurrentThreadId();
+
+        private static readonly object fileLock = new object();
+
+        // cpu counter
+        static void ThreadFunction1(object obj)
+        {
+            var (fileName, grp, id, randomCounter) = ((string, int, int, bool))obj;
+            uint nativeThreadId = GetCurrentThreadId();
+
+            lock (fileLock)
+            {
+                File.AppendAllText(fileName, $"{Process.GetCurrentProcess().Id}, {GetCurrentThreadId()}, {Thread.CurrentThread.ManagedThreadId}. {grp}, {id}\n");
+            }
+
+            var denominator = 2;
+            if (randomCounter)
+            {
+                denominator = (new Random()).Next(2, 6);
+            }
+            var counter = int.MaxValue / denominator;
+            Console.WriteLine($"id={id}, denominator={denominator} ");
+            while (counter-- > 0)
+                ;
+        }
+
+        static void RunWithOptions(Options opts)
+        {
+            Console.WriteLine($"Group number set to: {opts.Group}");
+
+            string fileName = $"threads_{Process.GetCurrentProcess().Id}.csv";
+            lock (fileLock)
+            {
+                File.WriteAllText(fileName, "Process ID, Native Thread ID, Managed Thread ID, Group, Serial Number\n");
+            }
+
+            // cpu counter
+            for (int grp = 0; grp < opts.Group; grp++)
+            {
+                var thdCount = (int)Math.Pow(2, grp);
+                var threads = new List<Thread>();
+                for (int j = 0; j < thdCount; j++)
+                {
+                    var thread = new Thread(new ParameterizedThreadStart(ThreadFunction1));
+                    thread.Start((fileName, grp, j, opts.IsRandomCounter));
+                    threads.Add(thread);
+                }
+                foreach (var thread in threads)
+                {
+                    thread.Join();
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(opts => RunWithOptions(opts))
+                .WithNotParsed<Options>(errs => HandleParseError(errs));
+        }
+    }
+}
